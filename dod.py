aad81be901471d53577b652b92537adbbe145330
Przemek <alex_spellman1@outlook.com>
2022-05-09 12:00:26 +0200

    first init

---
+++ dod.py
@@ -0,0 +1,23 @@
+import pandas as pd
+with open("logs.txt","r") as f:
+    with open("wynik.txt","a") as w:
+        plik = f.readlines()
+        i = 0
+        new_line = ""
+        for line in plik:
+            line = line.split("|")
+            
+            if i ==1:
+                new_line=line
+            elif i ==0:
+                columns = line
+                # df = pd.DataFrame()
+                # df.to_csv("wynik_csv.csv",columns=line,index=False)
+            else:
+                pass
+            
+            i+=1
+            if i>10:
+                break
+                
+        print(new_line)
