33554a805a8368eaeddeb4c3a6f8344ee62ad4c4
Przemek <alex_spellman1@outlook.com>
2022-05-16 11:31:45 +0200

    changed logs.csv

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pandas as pd
-dane = pd.read_csv("logs_converted.csv")
+dane = pd.read_csv("logs_converted.csv",header=None, skiprows=[0])
 print(dane.head(10))
+
+
+
+
+
+# cities.to_csv('cities.csv', index=False, sep=';')
