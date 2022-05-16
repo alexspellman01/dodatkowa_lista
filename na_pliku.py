91201f6fd497df0d19eb43cd643c09f5eca8f2f7
K4M1X <55177771+K4M1X@users.noreply.github.com>
2022-05-16 11:57:47 +0200

    k

--- na_pliku.py
+++ na_pliku.py
@@ -1,28 +1,27 @@
 import pandas as pd
-with open("logs.txt","r") as f:
-    with open("wynik.txt","a") as w:
+
+with open("logs.txt", "r") as f:
+    with open("wynik.txt", "a") as w:
         plik = f.readlines()
         i = 0
         new_line = ""
         for line in plik:
             line = line.split("|")
-            
-            if i ==1:
-                non_repeated_line=line
-            elif i ==0:
+
+            if i == 1:
+                non_repeated_line = line
+            elif i == 0:
                 columns = line
                 # df = pd.DataFrame() dodane
                 # df.to_csv("wynik_csv.csv",columns=line,index=False)
             else:
                 if line[0] == non_repeated_line[0]:
-                    non_repeated_line[3]=float(line[3]) +float(non_repeated_line[3]) #duaration 
+                    non_repeated_line[3] = float(line[3]) + float(non_repeated_line[3])  # duaration
                     print(non_repeated_line)
-                    
-            
-            i+=1
-            if i>10:
+
+            i += 1
+            if i > 10:
                 break
-                
+
         print(new_line)
 
-     
